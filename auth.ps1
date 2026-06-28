d0af2426b9f710ee45ae777c3f1970ba8c748b36
enterpriseBerreth <enterpriseBerreth@users.noreply.github.com>
2026-06-28 17:27:40 -0600

    chore: add Railway deployment config

---
+++ auth.ps1
@@ -0,0 +1,4 @@
+& "C:\Program Files\GitHub CLI\gh.exe" auth login --hostname github.com --git-protocol https --web
+Write-Host ""
+Write-Host "Auth complete. You can close this window."
+Read-Host "Press Enter to exit"
